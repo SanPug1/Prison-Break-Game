1dd5ed8f890682f983187b9714746bf7c2b35ade
InfiniteCheckerboard <silentsong0@protonmail.com>
2023-03-10 10:09:08 -0500

    Added Player Scene & Simple Player Controls

---
+++ Entities/Player.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+
+export(float) var player_speed = 15
+export(float,0,1) var horizontal_drag=0.93
+
+var in_air = true
+var moved_this_frame = false
+
+func _ready():
+	pass
+
+func _process(delta):
+	var controlledForce = Vector2.ZERO
+	moved_this_frame = false
+	if Input.is_action_pressed("player_left"):
+		controlledForce.x -= player_speed
+		moved_this_frame = true
+	if Input.is_action_pressed("player_right"):
+		controlledForce.x += player_speed
+		moved_this_frame = true
+	apply_central_impulse(controlledForce)
+
+func _integrate_forces(state):
+	if (!moved_this_frame):
+		linear_velocity.x *= horizontal_drag
