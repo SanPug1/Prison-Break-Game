8d1a46635b7b383c3a20900894d09a00a9b18ac1
SanPug1 <125669770+SanPug1@users.noreply.github.com>
2023-03-11 16:22:53 -0500

    audio added to title screen

--- Main Menu/Title Screen.gd
+++ Main Menu/Title Screen.gd
@@ -3,12 +3,16 @@ extends Node
 var pressedbutton = false
 
 func _ready():
-	pass # Replace with function body.
+	$IntroGameMusic.play()
 
+func _on_IntroGameMusic_finished():
+	if pressedbutton == false:
+		$IntroGameMusic.play()
 
 func _on_StartButton_button_up():
 	$MessageLabel.hide()
 	$StartButton.hide()
 	$ColorRect.hide()
+	$IntroGameMusic.stop()
 	pressedbutton = true
-	
+
