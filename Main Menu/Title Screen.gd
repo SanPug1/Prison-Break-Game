cd8da49d48e2507a67e22955acf5ca3226be1bdd
SanPug1 <125669770+SanPug1@users.noreply.github.com>
2023-03-11 17:47:54 -0500

    title screen visual update

--- Main Menu/Title Screen.gd
+++ Main Menu/Title Screen.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var pressedbutton = false
+var visiblebutton = true
 
 func _ready():
 	$IntroGameMusic.play()
@@ -11,8 +12,22 @@ func _on_IntroGameMusic_finished():
 
 func _on_StartButton_button_up():
 	$MessageLabel.hide()
+	$GameNameLabel.hide()
 	$StartButton.hide()
-	$ColorRect.hide()
+	$MainMenuBackground.hide()
 	$IntroGameMusic.stop()
+	$TextTimer.stop()
 	pressedbutton = true
 
+
+
+func _on_TextTimer_timeout():
+	if visiblebutton == false and pressedbutton == false:
+		visiblebutton = true
+		$MessageLabel.show()
+	elif visiblebutton == true and pressedbutton == false:
+		visiblebutton = false
+		$MessageLabel.hide()
+	else:
+		$MessageLabel.hide()
+		
