1cf10e11d43dc4b8a0616615e938697cf919eb5b
SanPug1 <125669770+SanPug1@users.noreply.github.com>
2023-03-15 19:17:43 -0400

    character and tile set updates

--- Player_Mel_Version.gd
+++ Player_Mel_Version.gd
@@ -1,35 +1,37 @@
-extends Area2D
+extends RigidBody2D
 
-export var speed = 200
-export var jumpspeed = 300
+var onthefloor = true
 var screen_size = Vector2.ZERO
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
-func _process(delta):
+func _physics_process(delta):
 	var velocity = Vector2.ZERO
+	var verticalvelocity = get_linear_velocity().y
 	
 	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_d"):
 		velocity.x += 1
-	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_a"):
+		set_linear_velocity(Vector2(200,verticalvelocity))
+	elif Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_a"):
 		velocity.x -= 1
-	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_w"):
-		velocity.y -= 1
-	if Input.is_action_pressed("ui_down") or Input.is_action_pressed("ui_s"):
-		velocity.y += 1
+		set_linear_velocity(Vector2(-200,verticalvelocity))
+	else:
+		set_linear_velocity(Vector2())
 	
+	if Input.is_action_just_pressed("ui_space"):
+		apply_impulse(Vector2(),Vector2(0,-150))
+		
 	if velocity.length() > 0:
 		velocity = velocity.normalized()
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-		
-	position += velocity * speed * delta
-	position.x = clamp(position.x,0,screen_size.x)
-	position.y = clamp(position.y,0,screen_size.y)
-	
+
+	position.x = clamp(position.x,15,screen_size.x-15)
+	position.y = clamp(position.y,0,screen_size.y-55)
+
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "walking"
 		$AnimatedSprite.flip_h = velocity.x > 0
-		
+
