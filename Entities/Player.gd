b946aba6ed43dba0bb0d617c69c4514c5c5864fa
InfiniteCheckerboard <silentsong0@protonmail.com>
2023-03-16 09:40:16 -0400

    Tweaks to player code

--- Entities/Player.gd
+++ Entities/Player.gd
@@ -24,27 +24,24 @@ func _process(_delta):
 	if can_jump and Input.is_action_pressed("player_jump"):
 		prepared_motion_this_frame.y -= jump_strength
 		can_jump = false
+	
+	# Apply Motion Multipliers
+	if (prepared_motion_this_frame.x > 0):
+		prepared_motion_this_frame.x *= postitive_motion_multiplier.x
+	else:
+		prepared_motion_this_frame.x *= negative_motion_multiplier.x
+	if (prepared_motion_this_frame.y > 0):
+		prepared_motion_this_frame.y *= postitive_motion_multiplier.y
+	else:
+		prepared_motion_this_frame.y *= negative_motion_multiplier.y
+	prepared_motion_this_frame.x *= all_motion_multiplier.x
+	prepared_motion_this_frame.y *= all_motion_multiplier.y
 
 func _integrate_forces(_state):
 	linear_velocity.x = prepared_motion_this_frame.x
 	linear_velocity.y += prepared_motion_this_frame.y
-	# Apply Motion Multipliers
-	
-	if (linear_velocity.x > 0):
-		linear_velocity.x *= postitive_motion_multiplier.x
-	else:
-		linear_velocity.x *= negative_motion_multiplier.x
-	
-	if (linear_velocity.y > 0):
-		linear_velocity.y *= postitive_motion_multiplier.y
-	else:
-		linear_velocity.y *= negative_motion_multiplier.y
-	
-	linear_velocity.x *= all_motion_multiplier.x
-	linear_velocity.y *= all_motion_multiplier.y
 
 func _on_Collision_Detected(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Detected")
 	var collider = shape_owner_get_owner(local_shape_index)
 	if collider == $Detect_Bottom:
 		_on_Down_Collide(body_rid, body, body_shape_index, local_shape_index)
@@ -58,25 +55,21 @@ func _on_Collision_Detected(body_rid, body, body_shape_index, local_shape_index)
 		_on_General_Collide(body_rid, body, body_shape_index, local_shape_index)
 
 func _on_Down_Collide(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Down")
 	can_jump = true
 
 func _on_Up_Collide(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Up")
+	pass
 
 func _on_Left_Collide(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Left")
 	negative_motion_multiplier.x = 0
 
 func _on_Right_Collide(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Right")
 	postitive_motion_multiplier.x = 0
 
 func _on_General_Collide(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision General")
+	pass
 
 func _on_Collision_Ended(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Ended")
 	var collider = shape_owner_get_owner(local_shape_index)
 	if collider == $Detect_Bottom:
 		_on_Down_Collide_End(body_rid, body, body_shape_index, local_shape_index)
@@ -90,20 +83,16 @@ func _on_Collision_Ended(body_rid, body, body_shape_index, local_shape_index):
 		_on_General_Collide_End(body_rid, body, body_shape_index, local_shape_index)
 
 func _on_Down_Collide_End(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Down")
 	can_jump = false
 
 func _on_Up_Collide_End(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Up")
+	pass
 
 func _on_Left_Collide_End(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Left")
 	negative_motion_multiplier.x = 1
 
 func _on_Right_Collide_End(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision Right")
 	postitive_motion_multiplier.x = 1
 
 func _on_General_Collide_End(body_rid, body, body_shape_index, local_shape_index):
-	print("Collision General")
 	pass
