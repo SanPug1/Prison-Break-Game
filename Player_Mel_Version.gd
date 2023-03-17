b407509073bbe9bbfc57a214064c588f8d25f204
SanPug1 <125669770+SanPug1@users.noreply.github.com>
2023-03-16 21:25:16 -0400

    player/map physics updated and WORKING :D

--- Player_Mel_Version.gd
+++ Player_Mel_Version.gd
@@ -1,37 +1,12 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-var onthefloor = true
-var screen_size = Vector2.ZERO
+export var speed: = Vector2(300.0,1000.0)
+export var gravity: = 3000.0
 
-func _ready():
-	screen_size = get_viewport_rect().size
+var velocity: = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	velocity.y = max(velocity.y, speed.y)
+	velocity = move_and_slide(velocity)
 	
-func _physics_process(delta):
-	var velocity = Vector2.ZERO
-	var verticalvelocity = get_linear_velocity().y
-	
-	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_d"):
-		velocity.x += 1
-		set_linear_velocity(Vector2(200,verticalvelocity))
-	elif Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_a"):
-		velocity.x -= 1
-		set_linear_velocity(Vector2(-200,verticalvelocity))
-	else:
-		set_linear_velocity(Vector2())
-	
-	if Input.is_action_just_pressed("ui_space"):
-		apply_impulse(Vector2(),Vector2(0,-300))
-		
-	if velocity.length() > 0:
-		velocity = velocity.normalized()
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
-
-	position.x = clamp(position.x,15,screen_size.x-15)
-	position.y = clamp(position.y,50,screen_size.y-50)
-
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "walking"
-		$AnimatedSprite.flip_h = velocity.x > 0
-
