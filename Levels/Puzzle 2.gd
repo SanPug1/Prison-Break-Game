fe07681b4ea481c2cb0da4b880b6aea3fd34cce4
CrestLac <tuanquanglac1312@gmail.com>
2023-03-25 16:05:41 -0400

    puzzle 2 design

--- Levels/Puzzle 2.gd
+++ Levels/Puzzle 2.gd
@@ -4,12 +4,13 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var array = []
+var inQueue = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	array.resize(8)
-	array.fill(Vector2.ZERO)
+	inQueue.resize(2)
+	inQueue.fill(null)
+	show()
 	pass # Replace with function body.
 
 
