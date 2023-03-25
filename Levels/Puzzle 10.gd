f6340f4ecae5dc9922cb858f222e9c05230c52e6
SanPug1 <125669770+SanPug1@users.noreply.github.com>
2023-03-25 16:05:32 -0400

    updated puzzle 10 and a working box YAY

--- Levels/Puzzle 10.gd
+++ Levels/Puzzle 10.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var firstrun = true
+var playerhaskey = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,16 +13,19 @@ func _process(delta):
 	if firstrun == true:
 		$TileMap.show()
 		$TileMap3.hide()
+		$keyArea.hide()
 		$Area2D.show()
 		$PressedButtonTile.hide()
 		$TileMap.set_collision_layer_bit(0, true)
 		$TileMap.set_collision_mask_bit(0, true)
 		$TileMap3.set_collision_layer_bit(0, false)
 		$TileMap3.set_collision_mask_bit(0, false)
+		$doorArea.hide()
 		firstrun = false
 
 func _on_ButtonTimer_timeout() -> void:
 	$TileMap3.hide()
+	$keyArea.hide()
 	$TileMap.show()
 	$PressedButtonTile.hide()
 	$Area2D.show()
@@ -35,6 +39,7 @@ func _on_Area2D_body_entered(body: Node) -> void:
 	$PressedButtonTile.show()
 	$TileMap.hide()
 	$TileMap3.show()
+	$keyArea.show()
 	$TileMap3.set_collision_layer_bit(0, true)
 	$TileMap3.set_collision_mask_bit(0, true)
 	$TileMap.set_collision_layer_bit(0, false)
@@ -42,3 +47,12 @@ func _on_Area2D_body_entered(body: Node) -> void:
 	
 func _on_Area2D_body_exited(body: Node) -> void:
 	$ButtonTimer.start()
+
+func _on_keyArea_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	playerhaskey = true
+	$doorArea.show()
+
+
+func _on_doorArea_body_entered(body: Node) -> void:
+	if playerhaskey == true and Input.is_action_just_released("interact"):
+		get_tree().change_scene("res://Levels/Puzzle 1.tscn")
