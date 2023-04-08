01fbbe4ae030d03c8e8264604188d99a31e6c5a3
SanPug1 <125669770+SanPug1@users.noreply.github.com>
2023-04-08 19:26:20 -0400

    changes to 4 platform level and size of light on dark moving platform level

--- Main.gd
+++ Main.gd
@@ -7,8 +7,11 @@ var Credits
 func _ready():
 	title = $Title_Screen
 	Credits = $Credits
+	levels = $Manager
 	remove_child($Credits)
+	remove_child($Manager)
 	
 func _on_Title_Screen_game_start() -> void:
 	remove_child($Title_Screen)
+	add_child(levels)
 	$Manager.start_playing()
