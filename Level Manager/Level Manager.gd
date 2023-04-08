85bb8779c030b074ab7aa1694b2a86ef32b05444
InfiniteCheckerboard <silentsong0@protonmail.com>
2023-04-08 17:48:34 -0400

    Moving Platform Bug Fixed

--- Level Manager/Level Manager.gd
+++ Level Manager/Level Manager.gd
@@ -5,18 +5,28 @@ export(Array, PackedScene) var levels_in_order
 var current_level_index = 0
 var current_level = null
 
+export(bool) var start_immediately = true
+
 func _ready():
-	$Music.play()
 	current_level_index = -1
-	load_next_level()
+	if start_immediately:
+		start_playing()
+
+func start_playing():
+	$Music.play()
+	current_level_index = 0
+	load_level(0)
 
 func load_next_level():
 	current_level_index += 1
-	if current_level_index >= len(levels_in_order):
+	load_level(current_level_index)
+
+func load_level(level):
+	if level >= len(levels_in_order) or level < 0:
 		return
 	if current_level != null:
 		current_level.queue_free()
-	current_level = levels_in_order[current_level_index].instance()
+	current_level = levels_in_order[level].instance()
 	$Level.add_child(current_level)
 
 func _on_music_finished():
--- Main.gd
+++ Main.gd
@@ -5,12 +5,10 @@ var title
 var Credits
 
 func _ready():
-	levels = $Manager
 	title = $Title_Screen
 	Credits = $Credits
 	remove_child($Credits)
-	remove_child($Manager)
 	
 func _on_Title_Screen_game_start() -> void:
-	add_child(levels)
 	remove_child($Title_Screen)
+	$Manager.start_playing()
